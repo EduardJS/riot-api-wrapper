767940444b9be1a27a2f6858bbe9ceea166914ae
Edi Duluman <edi@sqnp.net>
2016-06-23 22:49:52 +0300

    cleaned methods

--- classes/RiotApi.php
+++ classes/RiotApi.php
@@ -3,25 +3,18 @@
 class RiotApi {
 
 
-	const API_URL_1_1 = 'https://{region}.api.pvp.net/api/lol/{region}/v1.1/';
-	const API_URL_1_2 = 'https://{region}.api.pvp.net/api/lol/{region}/v1.2/';
-	const API_URL_1_3 = 'https://{region}.api.pvp.net/api/lol/{region}/v1.3/';
-	const API_URL_1_4 = 'https://{region}.api.pvp.net/api/lol/{region}/v1.4/';
-	const API_URL_2_1 = 'https://{region}.api.pvp.net/api/lol/{region}/v2.1/';
-	const API_URL_2_2 = 'https://{region}.api.pvp.net/api/lol/{region}/v2.2/';
-	const API_URL_2_3 = "https://{region}.api.pvp.net/api/lol/{region}/v2.3/";
-	const API_URL_2_4 = "https://{region}.api.pvp.net/api/lol/{region}/v2.4/";
-	const API_URL_2_5 = "https://{region}.api.pvp.net/api/lol/{region}/v2.5/";
-
-	const LONG_LIMIT_INTERVAL = 600;
-	const RATE_LIMIT_LONG = 500;
-
-	const SHORT_LIMIT_INTERVAL = 10;
-	const RATE_LIMIT_SHORT = 10;
-
-	const CACHE_LIFETIME_MINUTES = 60;
-
-	private $cache,
+	private $apiVersion = [
+				'1.1' => 'https://{region}.api.pvp.net/api/lol/{region}/v1.1/',
+				'1.2' => 'https://{region}.api.pvp.net/api/lol/{region}/v1.2/',
+				'1.3' => 'https://{region}.api.pvp.net/api/lol/{region}/v1.3/',
+				'1.4' => 'https://{region}.api.pvp.net/api/lol/{region}/v1.4/',
+				'2.1' => 'https://{region}.api.pvp.net/api/lol/{region}/v2.1/',
+				'2.2' => 'https://{region}.api.pvp.net/api/lol/{region}/v2.2/',
+				'2.3' => "https://{region}.api.pvp.net/api/lol/{region}/v2.3/",
+				'2.4' => "https://{region}.api.pvp.net/api/lol/{region}/v2.4/",
+				'2.5' => "https://{region}.api.pvp.net/api/lol/{region}/v2.5/"
+			],
+			$cache,
 			$region,
 			$key,
 			$responseCode,
@@ -48,30 +41,35 @@ class RiotApi {
 
 	}
 
-	public function getMatch($matchId, $timeLine=false) {
-		$call = self::API_URL_2_2  . 'match/' . $matchId . ($timeLine ? '?includeTimeline=true' : '');
-		return $this->request($call, $timeLine);
+	public function setRegion( $region )
+	{
+		$this->region = $region;
 	}
 
-	public function getMatchHistory( $id )
+	public function getMatch( $id, $params = [] )
 	{
-		return $this->request( self::API_URL_2_2  . 'matchlist/by-summoner/' . $id );
+		$call = $this->apiVersion['2.2']  . 'match/' . $id;
+		return $this->request( $call, $params );
+	}
+
+	public function getMatchHistory( $id, $params = [] )
+	{
+		return $this->request( $this->apiVersion['2.2']  . 'matchlist/by-summoner/' . $id, $params );
 	}
 
 	public function getGame( $id )
 	{
-
-		return $this->request( self::API_URL_1_3 . 'game/by-summoner/' . $id . '/recent' );
+		return $this->request( $this->apiVersion['1.3'] . 'game/by-summoner/' . $id . '/recent' );
 	}
 
-	public function getLeague( $id, $entry = null )
+	public function getLeague( $id )
 	{
-		return $this->request( self::API_URL_2_5 . 'league/by-summoner/' . $id . '/' . $entry) ;
+		return $this->request( $this->apiVersion['2.5'] . 'league/by-summoner/' . $id ) ;
 	}
 
 	public function getStats( $id, $option = 'summary' )
 	{
-		return $this->request( self::API_URL_1_3 . 'stats/by-summoner/' . $id . '/' . $option );
+		return $this->request( $this->apiVersion['1.3'] . 'stats/by-summoner/' . $id . '/' . $option );
 	}
 	
 	public function getSummonerId( $name )
@@ -109,18 +107,18 @@ class RiotApi {
 				break;
 		}
 
-		return $this->request( self::API_URL_1_4 . $call );
+		return $this->request( $this->apiVersion['1.4'] . $call );
 
 	}
 
 	public function getSummonerByName( $name )
 	{
-		return $this->request( self::API_URL_1_4 . 'summoner/by-name/' . rawurlencode( $name ) );
+		return $this->request( $this->apiVersion['1.4'] . 'summoner/by-name/' . rawurlencode( $name ) );
 	}
 
 	public function getTeam( $id )
 	{
-		return $this->request( self::API_URL_2_3 . 'team/by-summoner/' . $id );
+		return $this->request( $this->apiVersion['2.3'] . 'team/by-summoner/' . $id );
 	}
 
 	private function updateLimitQueue( $queue, $interval, $callLimit  ){
@@ -150,35 +148,35 @@ class RiotApi {
 
 	}
 
-	private function request( $call, $otherQueries = false, $static = false ) {
+	private function request( $call, $params = [] )
+	{
 
-		$url = $this->format_url( $call, $otherQueries );
+		$params['api_key'] = $this->key;
+
+		$url = str_replace( '{region}', $this->region, $call ) . '?' . http_build_query( $params );
 
 		if( $this->cache !== null && $this->cache->has( $url ) )
 			$result = $this->cache->get( $url );
-
 		else {
 
-			if ( !$static ) {
-				$this->updateLimitQueue($this->longLimitQueue, self::LONG_LIMIT_INTERVAL, self::RATE_LIMIT_LONG);
-				$this->updateLimitQueue($this->shortLimitQueue, self::SHORT_LIMIT_INTERVAL, self::RATE_LIMIT_SHORT);
-			}
+			$this->updateLimitQueue( $this->longLimitQueue, 600, 500 );
+			$this->updateLimitQueue( $this->shortLimitQueue, 10, 10 );
 
-			$ch = curl_init( $url);
-			curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false);
-			curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, false);			
-			$result = curl_exec( $ch);
-			$this->responseCode = curl_getinfo( $ch, CURLINFO_HTTP_CODE);
-			curl_close( $ch);
+			$ch = curl_init( $url );
+			curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+			curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+			curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, false );			
+			$result = curl_exec( $ch );
+			$this->code = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
+			curl_close( $ch );
 
-			if( $this->responseCode == 200 ) {
+			if( $this->code == 200 ) {
 
 				if( $this->cache !== null )
-					$this->cache->put( $url, $result, self::CACHE_LIFETIME_MINUTES * 60 );
+					$this->cache->put( $url, $result, 600 );
 				
 			} else
-				throw new Exception( $this->errorCodes[ $this->responseCode ] );
+				return [ 'error': $this->errorCodes[ $this->code ] ];
 		}
 
 		$this->decode && $result = json_decode( $result, true );
@@ -186,11 +184,4 @@ class RiotApi {
 		return $result;
 	}
 
-	private function format_url( $call, $otherQueries = false ){
-		return str_replace( '{region}', $this->region, $call ) . ( $otherQueries ? '&' : '?' ) . 'api_key=' . $this->key;
-	}
-
-	public function setRegion($region) {
-		$this->region = $region;
-	}
 }
