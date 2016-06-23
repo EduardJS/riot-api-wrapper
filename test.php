7f4456f0e1b4a1d9dc53a84519646aa53646f9c5
Edi Duluman <edi@sqnp.net>
2016-06-23 21:58:53 +0300

    added test file

---
+++ test.php
@@ -0,0 +1,22 @@
+<?php
+
+	define( 'PATH', dirname( __FILE__ ) );
+	header( 'Content-Type: application/json' );
+
+	spl_autoload_register( function( $class ){
+		require PATH . '/classes/' . $class .'.php';
+	});
+
+	$data = [
+		'summoner' 	=> 'yourSummonerName',
+		'server' 	  => 'eune' // https://developer.riotgames.com/api/methods for implementation availability
+	];
+
+	$api = new RiotApi( 'YOUR-API-KEY' ); // `new FileSystemCache('cache/')` as second param for using the caching system
+	$api->setRegion( 'eune' );
+
+	$summoner = $api->getSummonerByName( $data['summoner'] )[ $data['summoner'] ];
+	
+	//$history 	= $api->getMatchHistory( $summoner['id'] );
+
+	echo json_encode( $summoner, JSON_PRETTY_PRINT );
