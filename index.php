297dc7b78d27d7a0e5dd09da8ec49b59fbad4c28
Edi Duluman <edi@sqnp.net>
2016-06-25 19:16:21 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,38 @@
 <?php
-	
-	// remove these two in production enviroments
-	ini_set( 'display_errors', 'On' );
-	error_reporting( E_ALL );
 
 	define( 'PATH', dirname( __FILE__ ) );
-	header( 'Content-Type: application/json' );
+	define( 'ASSETS_PATH', PATH . '/assets' );
 
 	spl_autoload_register( function( $class ){
 		require PATH . '/classes/' . $class .'.php';
 	});
 
 	$data = [
-		'summoner' => 'summonerName',
-		'server'   => 'eune'
+		'summoner' 	=> 'summonerName',
+		'apiKey' 	=> 'your-API-key',
+		'server' 	=> 'eune'
 	];
 
-	$api = new RiotApi( 'your-api-key', new FileSystemCache('cache/') );
+	$api = new RiotApi( $data['apiKey'], new FSCache('cache/') );
 	$api->setRegion( $data['server'] );
 
 	// get summoner id
 	$summonerId = $api->getSummonerId( $data['summoner'] );
 
+	
+	// get current tier
+	$league = $api->getLeague( $summonerId, true );
+	$league = [
+		'tier' 		=> $league['tier'],
+		'division' 	=> $league['entries'][0]['division'],
+		'points' 	=> $league['entries'][0]['leaguePoints']
+	];
+	
+
 	// get match history
 	$history = $api->getMatchHistory( $summonerId, [ 
-		'rankedQueues' 	=> 'TEAM_BUILDER_DRAFT_RANKED_5x5', // get ranked games only
-		'beginTime' 	=> 1464782400000 // June 1st 2016
+		'rankedQueues' 	=> 'TEAM_BUILDER_DRAFT_RANKED_5x5',
+		'beginTime' 	=> ( time() - ( 86400 * 7 ) ) * 1000, // last 7 days
+		'beginIndex' 	=> 0,
+		'endIndex' 	=> 10 // last 10 games
 	]);
